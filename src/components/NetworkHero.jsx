40fc8c2c6e8b03ad69b035752c1b8132e9492edb
khurshid <Khurshid@qurilo.com>
2024-07-08 18:50:59 +0530

    update in networkhero component

--- src/components/NetworkHero.jsx
+++ src/components/NetworkHero.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import networkheroimg from "../assets/Images/networkHeroImg.png";
+import bannermobile from "../assets/Images/bannermobile.jpg";
+import { useMediaQuery } from "react-responsive";
 
 const NetworkHero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   return (
     <>
-      <div className="h-screen bg-gray-50 flex items-center">
+      {/* <div className="h-screen bg-gray-50 flex items-center"> */}
         <section
-          className="relative bg-cover h-screen  bg-center py-32 w-full"
+          className="relative bg-cover md:h-screen  bg-center py-32 w-full"
           style={{
-            backgroundImage: `url(${networkheroimg})`,
+            backgroundImage: `url(${isMobile ? bannermobile : networkheroimg})`,
           }}
         >
-        {/* Gradient Overlay */}
-        <div className="absolute inset-0">
+          {/* Gradient Overlay */}
+          <div className="absolute inset-0">
             <div
               className="absolute inset-0"
               style={{
@@ -24,13 +27,13 @@ const NetworkHero = () => {
           </div>
 
           <div className="container mx-auto text-left text-white relative z-10 ">
-            <div className="flex items-center">
-              <div className="">
+            <div className="flex items-center ">
+              <div className="md:text-left text-center">
                 <div className="md:mx-20">
                   <h1 className="md:text-5xl font-sans text-3xl font-medium mb-6">
                     Network Management
                   </h1>
-                  <p className="text-xl font-sans mb-12 md:w-1/2 w-full">
+                  <p className="text-xl font-sans mb-12 md:w-1/2 w-full ">
                     Our ERP implementation process begins with a thorough
                     assessment of your business processes and requirements. We
                     analyze existing workflows, identify pain points, and map
@@ -44,19 +47,19 @@ const NetworkHero = () => {
               </div>
               <div className=" md:block hidden pl-16">
                 <div>
-                  <button className="group font-sans relative h-16 w-56 overflow-hidden  rounded-l-2xl bg-white text-lg font-bold text-purple">
+                  <button className="cursor-auto group font-sans relative h-16 w-56 overflow-hidden  rounded-l-2xl bg-white text-lg font-bold text-purple">
                     Implementation
                     <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
                   </button>
                 </div>
                 <div>
-                  <button className="group font-sans mt-4 relative h-16 w-56 overflow-hidden rounded-l-2xl bg-white text-lg font-bold text-purple">
+                  <button className="group cursor-auto font-sans mt-4 relative h-16 w-56 overflow-hidden rounded-l-2xl bg-white text-lg font-bold text-purple">
                     Implementation
                     <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
                   </button>
                 </div>
                 <div>
-                  <button className="group font-sans relative mt-4 h-16 w-56 overflow-hidden rounded-l-2xl bg-white text-lg font-bold text-purple">
+                  <button className="group cursor-auto font-sans relative mt-4 h-16 w-56 overflow-hidden rounded-l-2xl bg-white text-lg font-bold text-purple">
                     Implementation
                     <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
                   </button>
@@ -65,7 +68,7 @@ const NetworkHero = () => {
             </div>
           </div>
         </section>
-      </div>
+      {/* </div> */}
     </>
   );
 };
