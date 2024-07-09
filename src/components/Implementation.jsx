e85b1bb6e4b61e2d832d59313b7c7a14a0fd346a
khurshid <Khurshid@qurilo.com>
2024-07-09 12:03:29 +0530

    update

--- src/components/Implementation.jsx
+++ src/components/Implementation.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import assessmentlogo from "../assets/Images/assesmenticon.png";
+import customizationlogo from "../assets/Images/customization.png";
+import implementationicon from "../assets/Images/implementationicon.png";
+import supporticon from "../assets/Images/supporticon.png";
 
 export default function Implementation() {
   const cardData = [
     {
       id: 1,
-      img: "https://randomuser.me/api/portraits/men/15.jpg",
+      img: implementationicon,
       heading: "Implementation",
       desc: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Proin turpis orci, maximus sed purus a, cursus scelerisque
@@ -13,8 +17,8 @@ export default function Implementation() {
     },
     {
       id: 2,
-      img: "https://randomuser.me/api/portraits/men/15.jpg",
-      heading: "Implementation",
+      img: assessmentlogo,
+      heading: "Assessment",
       desc: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Proin turpis orci, maximus sed purus a, cursus scelerisque
                     purus. Morbi molestie, odio at sagittis rhoncus, felis massa
@@ -22,8 +26,8 @@ export default function Implementation() {
     },
     {
       id: 3,
-      img: "https://randomuser.me/api/portraits/men/15.jpg",
-      heading: "Implementation",
+      img: customizationlogo,
+      heading: "Customization",
       desc: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Proin turpis orci, maximus sed purus a, cursus scelerisque
                     purus. Morbi molestie, odio at sagittis rhoncus, felis massa
@@ -31,8 +35,8 @@ export default function Implementation() {
     },
     {
       id: 4,
-      img: "https://randomuser.me/api/portraits/men/15.jpg",
-      heading: "Implementation",
+      img: supporticon,
+      heading: "Support",
       desc: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Proin turpis orci, maximus sed purus a, cursus scelerisque
                     purus. Morbi molestie, odio at sagittis rhoncus,
@@ -49,25 +53,26 @@ export default function Implementation() {
               <div
                 key={card.id}
                 className="relative w-full  group max-w-md min-w-0 mx-4 mt-20 mb-6 break-words bg-white border shadow-2xl dark:bg-gray-800 dark:border-gray-700 md:max-w-xs rounded-xl"
-                style={{ height: "25rem" }}
+                style={{ height: "20rem" }}
               >
                 <div className="pb-6">
                   <div className="flex flex-wrap justify-center">
-                    <div className="flex justify-center w-28">
+                    <div className="flex justify-center w-full">
                       <div className="relative">
                         <img
+                          width={120}
                           src={card.img}
-                          className="dark:shadow-xl border-white dark:border-gray-800 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]"
+                          className="dark:shadow-xl  border-white dark:border-gray-800 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]"
                         />
                       </div>
                     </div>
                   </div>
-                  <div className="mt-2 mt-20 text-center">
-                    <h3 className="mb-1 text-2xl font-bold leading-normal text-gray-700 dark:text-gray-300">
+                  <div className="mt-12 text-center">
+                    <h3 className="mb-1 pt-6 font-sans text-2xl font-bold leading-normal text-black dark:text-gray-300">
                       {card.heading}
                     </h3>
                   </div>
-                  <div className="pt-6 mx-6 mt-6 text-center border-t border-gray-200 dark:border-gray-700/50">
+                  <div className=" mx-6 mt-4 text-center  border-gray-200 dark:border-gray-700/50">
                     <div className="flex flex-wrap justify-center">
                       <div className="w-full px-6">
                         <p
@@ -76,7 +81,7 @@ export default function Implementation() {
                             textOverflow: "ellipsis",
                             // whiteSpace: "nowrap",
                           }}
-                          className="mb-4 font-light leading-relaxed text-gray-600 dark:text-gray-400"
+                          className="mb-4 font-sans text-[14px]  font-light leading-relaxed text-black dark:text-gray-400"
                         >
                           {card.desc}
                         </p>
