4320c84be584a601521fcd7632f13695ae9441e2
khurshid <Khurshid@qurilo.com>
2024-07-11 12:05:54 +0530

    update in cardhover

--- src/components/cards/CardHover.jsx
+++ src/components/cards/CardHover.jsx
@@ -7,7 +7,7 @@ export default function CardHover() {
     <>
       <div className="relative w-72 h-[23.5rem] rounded-lg">
         <div className="absolute inset-0 bg-gradient-to-br transform translate3d-0 scale-[0.95] blur-3xl z-[-1]"></div>
-        <div className="card shadow-md relative w-full h-full overflow-hidden rounded-lg">
+        <div className="card shadow-sm relative w-full h-full overflow-hidden rounded-lg">
           <div
             style={{
               backgroundImage: `url(${"https://www.valuecoders.com/wp-content/uploads/2024/07/Startups.png"})`,
@@ -19,9 +19,9 @@ export default function CardHover() {
           
           </div>
           
-          <div className="content absolute top-0 left-0 flex flex-col   w-full h-full gap-2 p-5 text-center text-[#e8e8e8] rounded opacity-0 pointer-events-none translate-y-12 transition-all duration-600 cubic-bezier[0.23,1,0.32,1]">
-            <p className="md:text-2xl  text-left font-bold font-sans">Card Hover</p>
-            <p className="text-left md:text-[1.2rem] font-sans text-gray-500 ">
+          <div className="content absolute top-0 left-0 flex flex-col mt-6  w-full h-full gap-2 p-5 text-center rounded opacity-0 pointer-events-none translate-y-12 transition-all duration-600 cubic-bezier[0.23,1,0.32,1]">
+            <p className="md:text-2xl  text-left font-bold font-sans text-gray-700">Startups</p>
+            <p className="text-left mt-4 md:text-base font-sans text-gray-500 ">
               Accelerate your startup’s growth with tailor-made eCommerce
               solutions designed to streamline operations and scale your
               business effectively.
