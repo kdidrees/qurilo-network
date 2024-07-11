5a8eb7d97887956585fc819be20ceda990ab22ec
khurshid <Khurshid@qurilo.com>
2024-07-11 18:33:20 +0530

    update in the developmentprocess component

--- src/components/process/DevelopmentProcess.jsx
+++ src/components/process/DevelopmentProcess.jsx
@@ -5,44 +5,30 @@ const DevelomentProcess = () => {
   return (
     <>
       <div className="w-full pt-5 pb-9 bg-[#f5f5f5]">
-        <div className="w-[60%] mx-auto pt-10">
-          <div className="flex justify-between items-center">
-            <div className="border-[1px] border-gray-950  rounded-xl w-[30%]" />
-            <div className=" text-black md:text-3xl font-semibold">
+      <div className=" text-black md:text-3xl text-xl md:mt-4 font-sans font-semibold text-center">
               Process We Follow
             </div>
-            <div className="border-[1px] border-gray-950 rounded-xl w-[30%]" />
-          </div>
+
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-6 gap-3 mx-16 md:mt-4 mt-8">
+          {DevelopmentProcessData.map((devdata) => {
+            return (
+              <>
+                <div
+                  key={devdata.id}
+                  className=" mx-2 bg-white w-full md:my-8 my-2 relative text-center z-10   py-6 px-12 shadow-md rounded-xl"
+                >
+                  <div className="absolute flex justify-center items-center -top-4 left-[45%] bg-white rounded-full w-8 h-8">
+                    <div className="border-4 rounded-full p-1 w-2 h-2  border-black"></div>
+                  </div>
+                  <h1 className="text-xl font-medium my-4">Planning</h1>
+                  <p className="md:text-base text-center">{devdata.desc}</p>
+                
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
-  
-          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-6 gap-3 mx-16">
-            {DevelopmentProcessData.map((devdata) => {
-              return (
-                <>
-                  <div key={devdata.id} className=" mx-2 bg-white w-full my-8 relative text-center z-10   py-6 px-12 shadow-md rounded-xl">
-                    <div className="absolute flex justify-center items-center -top-4 left-[45%] bg-white rounded-full w-8 h-8">
-                      <div className="border-4 rounded-full p-1 w-2 h-2  border-black"></div>
-                    </div>
-                    <h1 className="text-xl font-medium my-4">Planning</h1>
-                    <p className="md:text-base text-center">
-                     {devdata.desc}
-                    </p>
-                    {/* <div className="absolute left-[-9px] xl:flex    hidden justify-center  z-0 -top-9 border-l-[1px] border-r-[1px] w-[19rem] border-t-[1px] border-dashed border-[rgba(110,8,84,1)] h-10 rounded-3xl">
-                    <MdPlayArrow
-                      className="text-[rgba(110,8,84,1)]  -mt-[0.8rem]"
-                      size={25}
-                    />
-                  </div> */}
-                  </div>
-
-              
-                </>
-              );
-            })}
-          </div>
-     
-  
     </>
   );
 };
