3fc68aae9a7676f8984ca7fc33e638558d12110b
khurshid <Khurshid@qurilo.com>
2024-07-12 15:46:58 +0530

    update in blogs

--- src/components/blog/Blogs.jsx
+++ src/components/blog/Blogs.jsx
@@ -1,29 +1,33 @@
-
 // import Title from './Title'
-import { blogData } from '../../data/applicationDevelopment'
-import BlogCards from './BlogCards';
+import { blogData } from "../../data/applicationDevelopment";
+import BlogCards from "./BlogCards";
+import Heading from "../heading/Heading";
 
 const Blogs = () => {
   return (
-    <div className='my-9'>
+    <div className="my-9 md:mt-24">
       {/* <Title heading="Blog Posts" subheading="Want to learn about technological stuff? Go ahead and educate yourself."/> */}
-       <div className='grid mt-8 w-[85%] mx-auto lg:grid-cols-4 grid-cols-1 gap-5'>
-        <div className='col-span-2 '>
-            <BlogCards data={blogData[0]} type="bigCard"/>
+      <Heading
+        title={`Blog Posts`}
+        desc={`Want to learn about technological stuff? Go ahead and educate yourself.`}
+      />
+      <div className="grid md:mt-16 mt-8 w-[85%] mx-auto lg:grid-cols-4 grid-cols-1 gap-5 ">
+        <div className="col-span-2 ">
+          <BlogCards data={blogData[0]} type="bigCard" />
         </div>
-        <div className='col-span-2 lg:flex flex-col gap-y-5 hidden  '>
-            {blogData.slice(1, 3).map((data, index) => (
-                <BlogCards key={index} data={data} type="smallCard"/>
-            ))}
+        <div className="col-span-2 lg:flex flex-col gap-y-5 hidden  ">
+          {blogData.slice(1, 3).map((data, index) => (
+            <BlogCards key={index} data={data} type="smallCard" />
+          ))}
         </div>
-        <div className='col-span-2 lg:hidden flex flex-col gap-y-5   '>
-            {blogData.slice(1, 3).map((data, index) => (
-                <BlogCards key={index} data={data} type="bigCard"/>
-            ))}
+        <div className="col-span-2 lg:hidden flex flex-col gap-y-5   ">
+          {blogData.slice(1, 3).map((data, index) => (
+            <BlogCards key={index} data={data} type="bigCard" />
+          ))}
         </div>
-       </div>
-</div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
