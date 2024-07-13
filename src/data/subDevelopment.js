6616623153252e9ffb98f60a5184f3f5eff57572
khurshid <Khurshid@qurilo.com>
2024-07-13 11:09:47 +0530

    updated

--- src/data/subDevelopment.js
+++ src/data/subDevelopment.js
@@ -10,31 +10,31 @@ export const subdevData = [
   {
     id: 2,
     icon: firsticon,
-    heading: "Custom Web Development",
+    heading: "Web Application Development",
     desc: `Provides advanced web development and programming services based on the latest technologies and industry trends.`,
   },
   {
     id: 3,
     icon: firsticon,
-    heading: "Custom Web Development",
+    heading: "Content And Document Management System",
     desc: `Provides advanced web development and programming services based on the latest technologies and industry trends.`,
   },
   {
     id: 4,
     icon: firsticon,
-    heading: "Custom Web Development",
+    heading: "Web Portal Development",
     desc: `Provides advanced web development and programming services based on the latest technologies and industry trends.`,
   },
   {
     id: 5,
     icon: firsticon,
-    heading: "Custom Web Development",
+    heading: "E-Commerce Solutions",
     desc: `Provides advanced web development and programming services based on the latest technologies and industry trends.`,
   },
   {
     id: 6,
     icon: firsticon,
-    heading: "Custom Web Development",
+    heading: "Social Networking",
     desc: `Provides advanced web development and programming services based on the latest technologies and industry trends.`,
   },
 ];
