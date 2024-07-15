f9e200ab4a54cb8904cd5f6d115fcf55cdc82f86
khurshid <Khurshid@qurilo.com>
2024-07-15 17:18:41 +0530

    udpate

--- src/data/webDev.js
+++ src/data/webDev.js
@@ -6,82 +6,128 @@ import fourthicon from "../assets/web-development/icons/fourth.png";
 import fifthicon from "../assets/web-development/icons/fifth.png";
 import sixthicon from "../assets/web-development/icons/sixth.png";
 
-
-// import grd images here 
+// import grd images here
 import ecommerceimg from "../assets/web-development/images/gridimages/ecommerce.png";
 import elearningimg from "../assets/web-development/images/gridimages/elearning.png";
 import reservationimg from "../assets/web-development/images/gridimages/reservation.png";
 import contentmanagementimg from "../assets/web-development/images/gridimages/contentmanagement.png";
 
-
 export const heroSectionData = {
   id: 1,
   heroimg: heroimg,
   heromobile: heromobile,
-  heading: "Backend Development Services",
-  desc: `Struggling with outdated backend development solutions? Upgrade to enterprise-grade backend development services from Qurilo. Our experienced team ensures secure and stable websites using the latest technologies. We offer web/backend backend, ERP/CRM backend, API integration, and backend refactoring.`,
+  heading: "Custom Web App Development Services",
+  desc: `Finding it tough to stand out online? ValueCoders custom web application development services can help. With innovative solutions tailored to your unique needs, we empower your online presence to capture attention and drive success.`,
   btn: "Contact Us",
 };
 
-export const backendServicesData = {
-  heading: "Our Backend Development Services",
-  desc: `As a leading backend web development company in India, we specialize in building best-in-class backend solutions that bring value to your business. Our innovative and creative teams deliver projects with excellence.`,
+export const webServicesData = {
+  heading: "Success-Driven Services for Your Business",
+  desc: `Experience the best of full-stack web development to ensure growth-centric digital transformation for your business. Our professional web development services include robust web solutions ranging from e-commerce portals, CMS, and ERP solutions to chatbots, custom apps, and more.`,
   img: "", // strategy image here
   cardData: [
     {
       id: 1,
-      icon:firsticon,
-      heading: "Web Application Backend Development",
-      desc: `We help businesses build robust web-based applications backend that enables them to create seamless user experience through their applications, using the latest technology trends while keeping your costs low.`,
+      icon: firsticon,
+      heading: "Web App Development",
+      desc: `We specialize in making custom web apps to solve unique business challenges. Our web app development services not only deliver what you need but also improve your online presence, helping you stand out in the crowded internet market.`,
     },
     {
       id: 2,
       icon: secondicon,
-      heading: "Mobile App Backend Development",
-      desc: `Get intuitive & intelligent information flow for your backend app users with our backend development services. We provide a seamless experience across multiple devices and platforms by creating native or cross-platform systems.`,
+      heading: "Frontend Development",
+      desc: `Our web development company analyzes your target audience insights, brand identity, and industry best practices to create an appealing, intuitive frontend for your web app, ensuring a consistent UX with stunning animations and effects.`,
     },
     {
       id: 3,
       icon: thirdicon,
-      heading: "ERP/CRM Backend Development",
-      desc: `Our experts help you enhance your existing ERPs or CRMs to make them richer in terms of functionalities, reporting, and user experience. We use modern frameworks and technologies for our operations.`,
+      heading: "Backend Development",
+      desc: `Our engineers build a robust, secure core for your web application with a solid architecture that supports future growth. Our flexible backends integrate seamlessly with other systems, while advanced data structures offer actionable business insights.`,
     },
     {
       id: 4,
       icon: fourthicon,
-      heading: "API Integration Services",
-      desc: `As a leading backend development company, we offer reliable API development & integration services for our clients. Using this feature, they can easily customize their application. Moreover, it helps them to analyze data as per their project requirements.`,
+      heading: "eCommerce Development",
+      desc: `We’ve expanded our expertise from startups to custom eCommerce solutions for large-scale, high-growth businesses. We enhance efficiency with scalable microservices architectures and high automation of business processes.`,
     },
     {
       id: 5,
       icon: fifthicon,
-      heading: "Custom Backend Solutions",
-      desc: `We are one of the best custom back-end development company India. We develop custom-built backend solutions for your business that take ideas from inception to implementation with speed, accuracy, and affordability.`,
+      heading: "API Development",
+      desc: `Our custom web application development company ensures seamless communication and integration. Our API development services streamline processes, enhance functionality, and create a unified user experience.`,
     },
     {
       id: 6,
       icon: sixthicon,
-      heading: "Backend Refactoring",
-      desc: `Need an optimum app performance solution? Our dedicated teams help you redesign the entire solution infrastructure and leave frontend systems untouched so that it runs smoothly without any technical cost or code enhancement needed on our part.`,
+      heading: "Full Stack Development",
+      desc: `With expertise in end-to-end development, ValueCoders provides comprehensive solutions. Our full-stack development team covers every aspect to ensure optimal performance of your applications.`,
+    },
+    {
+      id: 7,
+      icon: "",
+      heading: "Cross Platform App Development",
+      desc: `Reach a wider audience with applications that work flawlessly on various platforms. Our custom web development services allows you to create cross-platform apps that expand your market presence efficiently.`,
+    },
+    {
+      id: 8,
+      icon: "",
+      heading: "Web Portal Development",
+      desc: `We develop specialized web portals tailored for diverse groups including customers, business partners, eCommerce users, patients, and vendors. Our portals seamlessly integrate with corporate systems, providing a reliable hub of current information and support.`,
+    },
+    {
+      id: 9,
+      icon: "",
+      heading: "QA & Testing",
+      desc: `Outsource a web application development company to test your web app throughout the Software Development Life Cycle (SDLC) and examine it post-development, focusing on the functional scope, performance, usability, security, and other important aspects.`,
+    },
+    {
+      id: 10,
+      icon: "",
+      heading: "Legacy Modernization",
+      desc: `We ensure your web app remains relevant by modernizing its architecture and refactoring its source code. Our experts enhance performance by transitioning to more powerful environments, adding new features, and repairing outdated UX/UI designs.`,
+    },
+    {
+      id: 11,
+      icon: "",
+      heading: "Architecture",
+      desc: `Our solution architects meticulously plan functional components, select optimal technologies, and ensure the feasibility of business logic implementation. They also map API connections within the web solution and external systems.`,
+    },
+    {
+      id: 12,
+      icon: "",
+      heading: "Business Analysis",
+      desc: `Our business analysts prioritize your target audience’s needs through requirements engineering, defining the solution’s scope. They serve as a bridge between business stakeholders and the IT team to maintain alignment.`,
+    },
+    {
+      id: 13,
+      icon: "",
+      heading: "Integration",
+      desc: `We configure APIs to seamlessly integrate your web app with corporate or third-party systems and services, ensuring instant data synchronization across platforms.`,
+    },
+    {
+      id: 14,
+      icon: "",
+      heading: "Continuous Support and Evolution",
+      desc: `We continuously improve your web-based software to stay efficient, competitive, and aligned with your evolving business needs. Our streamlined DevOps processes ensure quick updates and planned module releases.`,
     },
   ],
 };
 
-export const backendContactSectionData = {
-  heading: "Backend for the Future!",
+export const webContactSectionData = {
+  heading: "web for the Future!",
   desc: `Enable Seamless Integrations and Expand Reach.`,
   button: "Contact Us",
   img: ``,
 };
 
-export const backendDevelopmentProcessData = {
-  heading: "The Backend  Development Process",
+export const webDevelopmentProcessData = {
+  heading: "The web  Development Process",
   desc: `Let’s explore the essential steps involved in transforming your vision into a functional and user-friendly app.`,
   cardData: [
     {
       id: 1,
       heading: "Planning",
-      desc: `We define the backend application’s purpose, target audience, and desired UX.`,
+      desc: `We define the web application’s purpose, target audience, and desired UX.`,
     },
     {
       id: 2,
@@ -96,12 +142,12 @@ export const backendDevelopmentProcessData = {
     {
       id: 4,
       heading: "Testing",
-      desc: `Our testing team ensures the backend application functions flawlessly across all devices.`,
+      desc: `Our testing team ensures the web application functions flawlessly across all devices.`,
     },
     {
       id: 5,
       heading: "Launch",
-      desc: `Our professionals ensure the backend application is launched on time without any glitches.`,
+      desc: `Our professionals ensure the web application is launched on time without any glitches.`,
     },
     {
       id: 6,
@@ -111,10 +157,10 @@ export const backendDevelopmentProcessData = {
   ],
 };
 
-// backend data is not available on the value coders
-export const backendtechnologiesData = {
-  heading: "Our Comprehensive backend Technology Stack",
-  desc: `Discover the full range of technologies we use for backend projects.`,
+// web data is not available on the value coders
+export const webtechnologiesData = {
+  heading: "Our Comprehensive web Technology Stack",
+  desc: `Discover the full range of technologies we use for web projects.`,
   cardData: [
     {
       id: 1,
@@ -203,15 +249,15 @@ export const backendtechnologiesData = {
   ],
 };
 
-export const backendfaqData = {
+export const webfaqData = {
   heading: "FAQs",
   desc: ``, // desc is empty here
   faqdata: [
     {
       id: 1,
-      question: "Why should I choose Qurilo as my backend development partner?",
+      question: "Why should I choose Qurilo as my web development partner?",
       answer:
-        " You should select Qurilo as your backend development partner because we have a team of experienced developers proficient in various backend programming languages and frameworks. They also possess expertise in developing APIs, managing databases, and creating scalable systems.",
+        " You should select Qurilo as your web development partner because we have a team of experienced developers proficient in various web programming languages and frameworks. They also possess expertise in developing APIs, managing databases, and creating scalable systems.",
       list: [
         "Sound technical capabilities",
         "Strong engineering culture",
@@ -227,7 +273,7 @@ export const backendfaqData = {
       question:
         "How soon can I complete my back-end development project with Qurilo?",
       answer:
-        " The time required for backend development can vary depending on the project. For a simple CRUD application, it might only take a few weeks. But a more complex application with many endpoints and integrations could take several months. Below is a rough timeline for backend development:",
+        " The time required for web development can vary depending on the project. For a simple CRUD application, it might only take a few weeks. But a more complex application with many endpoints and integrations could take several months. Below is a rough timeline for web development:",
       list: [
         "Week 1: Develop the initial feature set and define the database structure.",
         "Week 2: Write the code to connect to the database and perform basic CRUD operations.",
@@ -239,34 +285,34 @@ export const backendfaqData = {
     {
       id: 3,
       question:
-        " What are your main technologies you offer for backend development?",
+        " What are your main technologies you offer for web development?",
       answer: `
-        Technology is always evolving, and new languages and frameworks are constantly emerging. As such, we make it a point to stay up-to-date on the latest developments in backend technology to always offer our clients the best possible solution.
+        Technology is always evolving, and new languages and frameworks are constantly emerging. As such, we make it a point to stay up-to-date on the latest developments in web technology to always offer our clients the best possible solution.
   <br/><br/>
-At our backend development company, we offer a variety of technologies for backend development, including Node.js, PHP, Python, and Ruby on Rails. We may use one or multiple technologies to create a robust and scalable backend solution depending on the project.
+At our web development company, we offer a variety of technologies for web development, including Node.js, PHP, Python, and Ruby on Rails. We may use one or multiple technologies to create a robust and scalable web solution depending on the project.
         `,
     },
     {
       id: 4,
-      question: "How much does it cost for backend development?",
-      answer: `The cost of backend development can vary depending on several factors, such as the project’s complexity, the scope of work, the technologies used, and the developers’ hourly rates. It is best to discuss your specific requirements to get a personalized quote. However, we charge $25-$30 hourly to develop the backend.`,
+      question: "How much does it cost for web development?",
+      answer: `The cost of web development can vary depending on several factors, such as the project’s complexity, the scope of work, the technologies used, and the developers’ hourly rates. It is best to discuss your specific requirements to get a personalized quote. However, we charge $25-$30 hourly to develop the web.`,
     },
     {
       id: 5,
       question:
-        "How does Qurilo ensure the quality and security of backend development projects?",
-      answer: `Qurilo is committed to delivering high-quality and secure backend development solutions. Here’s how we ensure it:`,
+        "How does Qurilo ensure the quality and security of web development projects?",
+      answer: `Qurilo is committed to delivering high-quality and secure web development solutions. Here’s how we ensure it:`,
       // put list items here
       ansList: [
         {
           id: 1,
           heading: `Experienced Developers`,
-          desc: `We have a team of experienced backend developers with expertise in various technologies and frameworks. They follow coding best practices and industry standards to deliver reliable and robust solutions.`,
+          desc: `We have a team of experienced web developers with expertise in various technologies and frameworks. They follow coding best practices and industry standards to deliver reliable and robust solutions.`,
         },
         {
           id: 2,
           heading: `Quality Assurance`,
-          desc: `We have a dedicated Quality Assurance (QA) team that conducts rigorous testing at every stage of the development process. This ensures that the backend code is thoroughly tested for functionality, performance, and security.`,
+          desc: `We have a dedicated Quality Assurance (QA) team that conducts rigorous testing at every stage of the development process. This ensures that the web code is thoroughly tested for functionality, performance, and security.`,
         },
         {
           id: 3,
@@ -276,7 +322,7 @@ At our backend development company, we offer a variety of technologies for backe
         {
           id: 4,
           heading: `Code Reviews `,
-          desc: `We conduct regular code reviews to maintain quality and identify potential issues or improvements. This helps ensure the backend code is clean, efficient, and maintainable.`,
+          desc: `We conduct regular code reviews to maintain quality and identify potential issues or improvements. This helps ensure the web code is clean, efficient, and maintainable.`,
         },
         {
           id: 5,
@@ -288,9 +334,9 @@ At our backend development company, we offer a variety of technologies for backe
   ],
 };
 
-export const backendDomainExpertiseData = {
-  heading: "Our Backend Development Expertise",
-  desc: `We specialize in building robust, scalable, and secure backend systems tailored to meet the needs of diverse industries. Our expertise ensures seamless integration, efficient data handling, and reliable performance for your applications.`,
+export const webDomainExpertiseData = {
+  heading: "Our web Development Expertise",
+  desc: `We specialize in building robust, scalable, and secure web systems tailored to meet the needs of diverse industries. Our expertise ensures seamless integration, efficient data handling, and reliable performance for your applications.`,
   data: [
     "API Development",
     "Database Design and Management",
@@ -299,5 +345,5 @@ export const backendDomainExpertiseData = {
     "Microservices Architecture",
     "Serverless Computing",
   ],
-  images: [ecommerceimg,elearningimg,reservationimg,contentmanagementimg],
+  images: [ecommerceimg, elearningimg, reservationimg, contentmanagementimg],
 };
