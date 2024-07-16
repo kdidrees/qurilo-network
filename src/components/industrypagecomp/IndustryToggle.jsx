2f59cd1914392cee6ab629a99062aeedd85ee26f
khurshid <Khurshid@qurilo.com>
2024-07-16 10:21:15 +0530

    updated

--- src/components/industrypagecomp/IndustryToggle.jsx
+++ src/components/industrypagecomp/IndustryToggle.jsx
@@ -4,18 +4,18 @@ export default function IndustryToggle({ data }) {
   return (
     <>
       <div className="max-w-screen-xl mx-auto px-5 bg-white min-h-sceen">
-        <div className="flex flex-col items-left lg:ml-12 md:mt-16 mt-4">
+        <div className="flex flex-col items-left lg:ml-12 md:mt-12 mt-4">
           <h2 className="text-black text-xl md:mt-6 mt-3">
             Fintech, Banking, Financial Services
           </h2>
         </div>
         <div className="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
           {data.map((item) => (
-            <div key={item.heading} className="py-5">
+            <div key={item.heading} className="py-3">
               <h3 className="font-medium">
                 {item.heading}
               </h3>
-              <p className="text-desc mt-3">
+              <p className="font-medium  font-sans text-gray-500 mb-6 dark:text-gray-400  mt-3">
                 {item.desc}
               </p>
             </div>
