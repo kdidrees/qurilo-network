28283e6bf71ca58095e721754b6c8ad32ee8dce7
khurshid <Khurshid@qurilo.com>
2024-07-16 10:23:43 +0530

    update

--- src/container/industry/IndustriesSection.jsx
+++ src/container/industry/IndustriesSection.jsx
@@ -5,32 +5,31 @@ import { industryToggleData } from "../../data/industrypage";
 
 export default function IndustriesSection() {
   return (
-
-      <section className="w-full bg-white lg:flex mb-10 lg:mt-10 ">
-        <div className="m-5 ml-8 lg:w-1/2">
-          <div className="mt-10 lg:ml-10 pb-6">
-            <h2
-              className=" font-semibold
-                  text-2xl
-                  font-sans
-                  md:text-3xl
-                  text-black
-                  mb-4 
-                  md:mt-0 mt-8"
-            > 
-            
-              Industries we Revolutionarize
-            </h2>
+    <>
+      {industryToggleData.map((item, index) => (
+        <section
+          key={index}
+          className={`w-full bg-white lg:flex mb-10 lg:mt-10 ${
+            index % 2 === 0 ? "flex-row" : "flex-row-reverse"
+          }`}
+        >
+          <div className="m-5 ml-8 lg:w-1/2">
+            <div className="mt-10 lg:ml-10 pb-6">
+              <h2
+                className="font-semibold text-2xl font-sans md:text-3xl text-black mb-4 md:mt-0 mt-8"
+              >
+                Industries we Revolutionarize
+              </h2>
+            </div>
+            <div>
+              <img src={industryimg} alt="" />
+            </div>
           </div>
-
-          <div>
-            <img src={industryimg} alt="" />
+          <div className="ml-8 lg:w-1/2 lg:mt-16 lg:mb-20">
+            <IndustryToggle data={[item]} />
           </div>
-        </div>
-        <div className="ml-8 lg:w-1/2 lg:mt-16 lg:mb-20 ">
-          <IndustryToggle data={industryToggleData} />
-        </div>
-      </section>
-    
+        </section>
+      ))}
+    </>
   );
 }
