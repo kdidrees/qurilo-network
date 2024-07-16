407fc6be48e61de8400825aaa45363408c5a4154
khurshid <Khurshid@qurilo.com>
2024-07-16 10:55:34 +0530

    update in the industry page

--- src/data/industrypage.js
+++ src/data/industrypage.js
@@ -1,22 +1,172 @@
 export const industryToggleData = [
   {
     id: 1,
-    heading: `What is a SAAS platform?`,
-    desc: `SAAS platform is a cloud-based software service that allows users to access and use a variety of tools and functionality.`,
+    heading: `Innovative Banking Solutions`,
+    data: [
+      {
+        id: 1,
+        heading: `Cloud`,
+        desc: `Financial institutions are making a shift towards cloud-based Software-as-a-Service (SaaS) applications for non-core business processes such as CRM, financial accounting, and KYC verification.`,
+      },
+
+      {
+        id: 2,
+        heading: `Conversational AI in Banking`,
+        desc: `Design thinking and FinTech innovation built around data and Artificial Intelligence (AI) can help create new models of customer engagement.`,
+      },
+
+      {
+        id: 3,
+        heading: `Blockchain`,
+        desc: `Several key attributes such as cryptographic security, built-in redundancy, and process integrity make the blockchain technology uniquely suited for enabling secure B2B interactions`,
+      },
+
+      {
+        id: 4,
+        heading: `Mobility enabled Cardless ATM`,
+        desc: `Bank are leveraging mobility solutions to break down the barriers of access to ATMs by enabling contactless transactions without the need of a debit or credit card .`,
+      },
+    ],
   },
   {
     id: 2,
-    heading: `What is a SAAS platform?`,
-    desc: `SAAS platform is a cloud-based software service that allows users to access and use a variety of tools and functionality.`,
+    heading: `Tech Innovation in Retail`,
+    data: [
+      {
+        id: 1,
+        heading: `Augmented Reality (AR) and O2O`,
+        desc: `Digitally-native e-commerce brands are testing Augmented Reality-enabled online-to-offline (O2O) experiences as the line between physical and digital commerce blurs.`,
+      },
+
+      {
+        id: 2,
+        heading: `Robotic Process Automation (RPA)`,
+        desc: `Although cost remains a top barrier in RPA implementations, more and more e-commerce businesses are expected to embrace automation to reduce their workforce.`,
+      },
+
+      {
+        id: 3,
+        heading: `Blockchain`,
+        desc: `Payments, supply chain management, and decentralized marketplaces are currently the three main use cases for blockchain technology in e-commerce.`,
+      },
+
+      {
+        id: 4,
+        heading: `Artificial Intelligence (AI)`,
+        desc: `E-Commerce brands are relying on natively-embedded automated fraud protection modules to prevent costly chargebacks.`,
+      },
+    ],
   },
+
   {
     id: 3,
-    heading: `What is a SAAS platform?`,
-    desc: `SAAS platform is a cloud-based software service that allows users to access and use a variety of tools and functionality.`,
+    heading: `Disrupting Non Profit, Education and Government Sectors`,
+    data: [
+      {
+        id: 1,
+        heading: `AR/VR`,
+        desc: `With AR VR Services stamp the learning that lasts long in the students’ minds and enable them to submerge themselves into simulative, interactive, and immersive learning experiences.`,
+      },
+
+      {
+        id: 2,
+        heading: `Intelligent Tutoring`,
+        desc: `Take full advantage of an automated, computer-based learning system that reduces the manual intervention of teachers and allows students to follow a customized learning approach.`,
+      },
+
+      {
+        id: 3,
+        heading: `AI-Based Education Apps`,
+        desc: `Free yourself from traditional education practices by building high-performant AI-based apps that simplify learning processes and enables educators to understand students’ need better.`,
+      },
+
+      {
+        id: 4,
+        heading: `Education & eLearning Chatbots`,
+        desc: `Build advanced conversational solutions that deliver human-like experiences to students, reduce ambiguity, and help learners pursue result-oriented conversations that lead to complete satisfaction.`,
+      },
+    ],
   },
   {
     id: 4,
-    heading: `What is a SAAS platform?`,
-    desc: `SAAS platform is a cloud-based software service that allows users to access and use a variety of tools and functionality.`,
+    heading: `Our Healthcare offerings`,
+    data: [
+      {
+        id: 1,
+        heading: `The Internet of Healthcare Things (IoHT)`,
+        desc: `Enable an integrated system of medical systems and applications to capture data that helps break silos, increases operational visibility, and provides for proactive monitoring.`,
+      },
+
+      {
+        id: 2,
+        heading: `Big Data and AI`,
+        desc: `Detect and analyze patterns across data in real-time to identify health conditions, potential treatment methods, and their outcomes—and take life-saving decisions at speed.`,
+      },
+
+      {
+        id: 3,
+        heading: `Distributed Ledger and Blockchain`,
+        desc: `Create a secure and standardized record of medical data as a way to ensure on-demand access to patient information and drive faster, more accurate supply chain analysis. `,
+      },
+
+      {
+        id: 4,
+        heading: `ARnatomy`,
+        desc: `Build and superimpose a 3D model of the patient’s anatomy on the patient’s body to perform surgeries with higher levels of precision and efficiency.`,
+      },
+    ],
+  },
+  {
+    id: 5,
+    heading: `All the best bits of Manufacturing & Chemicals`,
+    data: [
+      {
+        id: 1,
+        heading: "Materials management",
+        desc: "Our materials management service enables an integrated system to capture and analyze data, breaking silos and increasing operational visibility for proactive monitoring and efficient resource utilization.",
+      },
+      {
+        id: 2,
+        heading: "Production planning and control",
+        desc: "We offer production planning and control services that utilize real-time data analysis to optimize manufacturing processes, identify potential issues, and ensure seamless production flows.",
+      },
+      {
+        id: 3,
+        heading: "Sales and distribution",
+        desc: "Our sales and distribution service provides secure, standardized records of medical data, ensuring on-demand access to patient information and enhancing supply chain efficiency for timely and accurate deliveries.",
+      },
+      {
+        id: 4,
+        heading:
+          "Logistics, product life-cycle management and quality management",
+        desc: "We deliver comprehensive logistics, product lifecycle, and quality management services, utilizing advanced technologies to enhance precision in surgeries, ensure product integrity, and maintain high-quality standards throughout the product lifecycle.",
+      },
+    ],
+  },
+  {
+    id: 6,
+    heading: `Innovation in Hospitality`,
+    data: [
+      {
+        id: 1,
+        heading: "Artificial Intelligence (AI)",
+        desc: "Travel brands are embracing Artificial Intelligence (AI) to create tailored recommendations for travelers based on their previous travel history to drive bookings.",
+      },
+      {
+        id: 2,
+        heading: "Augmented Reality (AR) / Virtual Reality (VR)",
+        desc: "AR and VR technologies enable travel & hospitality businesses to enhance physical environments to encourage more tourists.",
+      },
+      {
+        id: 3,
+        heading: "Blockchain",
+        desc: "Blockchain’s immutable distributed ledger technology is isolating major hospitality industry pain points by removing the layers between consumers and sellers.",
+      },
+      {
+        id: 4,
+        heading: "Internet of Things (IoT)",
+        desc: "IoT technology is revolutionizing the hospitality space by helping the industry meet compliance requirements and asset tracking.",
+      },
+    ],
   },
 ];
