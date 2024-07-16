abfa98ad88ba041cfad2a2285e128f1be7aac0d4
khurshid <Khurshid@qurilo.com>
2024-07-16 15:43:47 +0530

    update

--- src/data/fullstackDevelopment.js
+++ src/data/fullstackDevelopment.js
@@ -1,25 +1,23 @@
+// import common images here
+import strategyimg from "../assets/common-images/strategy.png";
+import contactimg from "../assets/common-images/contactimg.png";
 
-// import common images here 
-import strategyimg from "../assets/common-images/strategy.png"
-import contactimg from "../assets/common-images/contactimg.png"
+// import hero images here
+import heroimg from "../assets/fullstack-development/images/Hire_Full_Stack_Developers.png";
+import heromoobileimg from "../assets/fullstack-development/images/hire_full_stack_developers_mobile.jpg";
 
-// import hero images here 
-import heroimg from "../assets/fullstack-development/images/Hire_Full_Stack_Developers.png"
-import heromoobileimg from "../assets/fullstack-development/images/hire_full_stack_developers_mobile.jpg"
+// import icons here
+import firsticon from "../assets/fullstack-development/icons/Full_Stack_Web_Development.png";
+import secondicon from "../assets/fullstack-development/icons/front_end_development.png";
+import thirdicon from "../assets/fullstack-development/icons/back_end_development.png";
+import fourthicon from "../assets/fullstack-development/icons/MEAN_MERN_Stack_Development.png";
+import fifthicon from "../assets/fullstack-development/icons/Full-stack_Support_and_Maintenance.png";
 
-// import icons here 
-import firsticon from "../assets/fullstack-development/icons/Full_Stack_Web_Development.png"
-import secondicon from "../assets/fullstack-development/icons/front_end_development.png"
-import thirdicon from "../assets/fullstack-development/icons/back_end_development.png"
-import fourthicon from "../assets/fullstack-development/icons/MEAN_MERN_Stack_Development.png"
-import fifthicon from "../assets/fullstack-development/icons/Full-stack_Support_and_Maintenance.png"
-
-// import grid images here 
-import firstimg from "../assets/fullstack-development/images/gridimages/API_Integration_Strategies.png"
-import secondimg from "../assets/fullstack-development/images/gridimages/Performance_Optimization_Techniques.png"
-import thirdimg from "../assets/fullstack-development/images/gridimages/Real-time_Data_Syncing.png"
-import fourthimg from "../assets/fullstack-development/images/gridimages/fullStack_Security_Protocols.png"
-
+// import grid images here
+import firstimg from "../assets/fullstack-development/images/gridimages/API_Integration_Strategies.png";
+import secondimg from "../assets/fullstack-development/images/gridimages/Performance_Optimization_Techniques.png";
+import thirdimg from "../assets/fullstack-development/images/gridimages/Real-time_Data_Syncing.png";
+import fourthimg from "../assets/fullstack-development/images/gridimages/fullStack_Security_Protocols.png";
 
 export const heroSectionData = {
   id: 1,
@@ -30,7 +28,7 @@ export const heroSectionData = {
   btn: "Contact Us",
 };
 
-export const fullStackServicesData = {
+export const servicesData = {
   heading: "Expertise of Our Full-Stack Developers",
   desc: `Our full-stack software engineers are experts in all stages of software development. Here are some of their core proficiencies:`,
   img: strategyimg, // strategy image here
@@ -59,7 +57,7 @@ export const fullStackServicesData = {
       heading: "MEAN & MERN Stack Development",
       desc: `Hire Full Stack developers having expertise in building web applications using MongoDB, ExpressJS, AngularJS, and Node.js. `,
     },
-  
+
     {
       id: 5,
       icon: fourthicon,
@@ -69,15 +67,19 @@ export const fullStackServicesData = {
   ],
 };
 
-export const fullStackContactSectionData = {
+export const contactOneData = {
   heading: "Try Before, Commit Later",
   desc: `Start your 7-day trial today and discover the perfect fit for your project needs.`,
   button: "Contact Us",
   img: contactimg,
 };
+export const contactTwoData = {
+  heading: "Build Smarter with Top Talent ",
+  desc: `Ready to elevate your software projects? Hire our expert developers and experience unparalleled innovation and efficiency. `,
+  button: "Book A Free Consultation",
+};
 
-
-export const fullStacktechnologiesData = {
+export const technologiesData  = {
   heading: "Our Comprehensive fullStack Technology Stack",
   desc: `Discover the full range of technologies we use for fullStack projects.`,
   cardData: [
@@ -209,7 +211,7 @@ export const fullStacktechnologiesData = {
   ],
 };
 
-export const fullStackfaqData = {
+export const faqData = {
   heading: "FAQs",
   desc: `Here are answers to some questions our clients frequently have in their minds:`,
   faqdata: [
@@ -219,34 +221,33 @@ export const fullStackfaqData = {
         "What are the benefits of hiring Full-stack engineers from ValueCoders?",
       answer:
         " When you hire a Full stack developer in India, you get complete peace of mind, on-time project execution, strict non-disclosure agreement, easy team scale up & down, and no contract lock-in. Also, you will have the opportunity to work with the top 1% of full-stack developers in India have 5+ years of average experience.",
-        ansList: [
-          {
-            id: 1,
-            heading: `Expertise`,
-            desc: `ValueCoders claims to have a team of experienced full-stack engineers with the knowledge and skills to handle various tasks and technologies.`,
-          },
-          {
-            id: 2,
-            heading: `Cost-effectiveness`,
-            desc: `Hire full-stack programmers from ValueCoders may be more cost-effective than hiring in-house full-stack engineers or using multiple specialized contractors.`,
-          },
-          {
-            id: 3,
-            heading: `Flexibility`,
-            desc: ` Full-stack engineers can work on various aspects of a project, which can be beneficial when you need to pivot or make changes to your project.`,
-          },
-          {
-            id: 4,
-            heading: `Communication `,
-            desc: `ValueCoders claims to have a team of professionals who are proficient in English and can communicate effectively with clients. This can be helpful in ensuring that your project is delivered to your specifications.`,
-          },
-        ],
+      ansList: [
+        {
+          id: 1,
+          heading: `Expertise`,
+          desc: `ValueCoders claims to have a team of experienced full-stack engineers with the knowledge and skills to handle various tasks and technologies.`,
+        },
+        {
+          id: 2,
+          heading: `Cost-effectiveness`,
+          desc: `Hire full-stack programmers from ValueCoders may be more cost-effective than hiring in-house full-stack engineers or using multiple specialized contractors.`,
+        },
+        {
+          id: 3,
+          heading: `Flexibility`,
+          desc: ` Full-stack engineers can work on various aspects of a project, which can be beneficial when you need to pivot or make changes to your project.`,
+        },
+        {
+          id: 4,
+          heading: `Communication `,
+          desc: `ValueCoders claims to have a team of professionals who are proficient in English and can communicate effectively with clients. This can be helpful in ensuring that your project is delivered to your specifications.`,
+        },
+      ],
     },
 
     {
       id: 2,
-      question:
-        "Will I have direct contact with the assigned developers?",
+      question: "Will I have direct contact with the assigned developers?",
       answer:
         "You will have direct contact with the assigned developers all the time. They are available on skype and other IM software for quick communication. We want you to be comfortable working with them and be able to get your questions answered quickly.",
     },
@@ -271,13 +272,13 @@ export const fullStackfaqData = {
         "Checking out the developer’s portfolio.",
         "Identifying current and past clients.",
         "Always look for the developer’s platform compatibility.",
-        "Always look out for clear communication channels"
+        "Always look out for clear communication channels",
       ],
     },
   ],
 };
 
-export const fullStackDomainExpertiseData = {
+export const domainExpertiseData = {
   heading: "Our fullStack Development Expertise",
   desc: `Explore our specialized capabilities in creating robust, scalable, and secure fullStack solutions tailored for diverse industries.`,
   data: [
@@ -290,5 +291,5 @@ export const fullStackDomainExpertiseData = {
     "Version Control and Maintenance",
     "Comprehensive Documentation Practices",
   ],
-  images: [firstimg,secondimg,thirdimg,fourthimg],
+  images: [firstimg, secondimg, thirdimg, fourthimg],
 };
