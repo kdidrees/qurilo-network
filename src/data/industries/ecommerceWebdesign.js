7aafff3fbb5c51425df744ae432099beb150d0dc
khurshid <Khurshid@qurilo.com>
2024-07-17 18:13:01 +0530

    update data

---
+++ src/data/industries/ecommerceWebdesign.js
@@ -0,0 +1,127 @@
+// import hero images here
+import heroimg from "../assets/backend-development/images/Backend_Development_Services.jpg";
+import heromobile from "../assets/backend-development/images/Backend_Development_Services_MOBILE.jpg";
+
+// import services icons here
+import firsticon from "../assets/backend-development/icons/web_application_backend_development.png";
+import secondicon from "../assets/backend-development/icons/mobile_app_backend_development.png";
+import thirdicon from "../assets/backend-development/icons/ERP_CRM_Backend_Development.png";
+import fourthicon from "../assets/backend-development/icons/api_integration.png";
+import fifthicon from "../assets/backend-development/icons/custom_backend_solutions.png";
+import sixthicon from "../assets/backend-development/icons/Backend_Refactoring.png";
+
+// grid images here
+import firstimg from "../assets/backend-development/images/gridimages/Authentication_and_AuthorizatioN.png";
+import secondimg from "../assets/backend-development/images/gridimages/Real-timeData_Processing.png";
+import thirdimg from "../assets/backend-development/images/gridimages/Serverless_Computing.png";
+import fourthimg from "../assets/backend-development/images/gridimages/cloud-computing-data-management-concept.jpg";
+
+// import common images here
+import contactimg from "../assets/common-images/contactimg.png";
+import strategyimg from "../assets/common-images/strategy.png";
+
+export const heroSectionData = {
+  id: 1,
+  heroDesktop: heroimg,
+  heroMobile: heromobile,
+  heading: "eCommerce Web Design Services",
+  desc: `Elevate your eCommerce website with ValueCoders’ design services. We will help you create user-friendly interfaces and optimize user experiences. Stand out in the crowded eCommerce landscape and elevate your business.`,
+  btn: "Contact Us",
+};
+
+export const servicesData = {
+  heading: "eCommerce Design Services",
+  desc: `
+
+
+  Our eCommerce design services are focused on creating visually stunning and user-friendly online stores that drive conversions. We work closely with our clients to understand their brand and target audience, and create designs that reflect their unique identity and values.
+
+`,
+  img: strategyimg,
+  list: [
+    {
+      id: 1,
+      icon: firsticon,
+      heading: "eCommerce Web Design",
+      desc: `We can help improve your site’s user experience and drive conversions with custom eCommerce website design services. Better user experience, improve functional flexibility, and incorporate advanced tech such as 3D modeling.`,
+    },
+    {
+      id: 2,
+      icon: secondicon,
+      heading: "eCommerce App Design",
+      desc: `We can help you retain your existing customers and attract new ones by delivering a fantastic experience through our UI/UX solutions. Improve customer acquisition & retention, better user engagement, and stay ahead of your competitors.`,
+    },
+    {
+      id: 3,
+      icon: thirdicon,
+      heading: "User Experience Upgrade",
+      desc: `We can assist you to keep up with the latest technologies with our eCommerce upgradation services on your existing eCommerce storefronts. Improve storefront UI/UX, leverage in-house usability testing, and enhance user experience.`,
+    }
+  ],
+
+};
+
+export const contactOneData = {
+  heading: "Let's Discuss Your Project",
+  desc: `Get a free consultation and let us know your project idea to turn it into an amazing digital product.`,
+  button: "Contact Us",
+  img: contactimg,
+};
+export const contactTwoData = {
+  heading: "Got a Project in Mind? Tell Us More",
+  desc: `Drop us a line, and we'll get back to you immediately to schedule a call and discuss your needs personally.`,
+  button: "Contact Us",
+};
+
+export const faqData = {
+  heading: "FAQs",
+  desc: `Here are answers to some questions our clients frequently have in their minds.`,
+  faqdata: [
+    {
+      id: 1,
+      question: "Why shall I choose ValueCoders for eCommerce design?",
+      answer:
+        " Choose ValueCoders for eCommerce design, as we have a skilled team of experienced designers who specialize in creating visually appealing and user-friendly eCommerce websites. Our focus on client satisfaction, cost-effective solutions, and timely project delivery ensures a seamless business experience.",
+    },
+
+    {
+      id: 2,
+      question:
+        "How much time does ValueCoders take for eCommerce web design?",
+      answer:
+        " The time taken for eCommerce web design at ValueCoders can vary depending on the project’s complexity and specific requirements. We work diligently to deliver projects within agreed timelines, ensuring efficient and high-quality solutions.",
+    },
+    {
+      id: 3,
+      question: " How many years of experience does your eCommerce design team have?",
+      answer: `
+      Our eCommerce design team at ValueCoders possesses extensive experience in the field, with an average experience of over 5 years. They are well-versed in the latest design trends and technologies to create cutting-edge eCommerce websites.
+        `,
+    },
+    {
+      id: 4,
+      question:
+        "How much do eCommerce web design services cost?",
+      answer: ` The cost of eCommerce web design services can vary based on project scope and features. At ValueCoders, we offer flexible engagement models and competitive pricing to provide cost-effective solutions without compromising quality. The cost of eCommerce web design services can vary based on project scope and features. At ValueCoders, we offer flexible engagement models and competitive pricing to provide cost-effective solutions without compromising quality. The cost of eCommerce web design services can vary based on project scope and features. At ValueCoders, we offer flexible engagement models and competitive pricing to provide cost-effective solutions without compromising quality.`,
+    },
+    {
+      id: 5,
+      question:
+        " Can I change the eCommerce web design platform by keeping the same design and functionalities?",
+      answer: ` Yes, our team can assist in migrating your eCommerce website to a different platform while preserving the same design and functionalities. We ensure a seamless transition with minimal disruption to your business operations.`,
+    },
+  ],
+};
+
+export const domainExpertiseData = {
+    heading: "eCommerce Web Design",
+    desc: `We can help improve your site’s user experience and drive conversions with custom eCommerce website design services. Better user experience, improve functional flexibility, and incorporate advanced tech such as 3D modeling.`,
+    points: [
+      "Better user experience",
+      "Improve functional flexibility",
+      "Incorporate advanced tech such as 3D modeling",
+    ],
+    images: [firstimg, secondimg, thirdimg], 
+};
+
+  
