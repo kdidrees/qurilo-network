a11bc3db28d1ebbe41c2b8831df8f6d4c24fa8b8
khurshid <Khurshid@qurilo.com>
2024-07-18 18:55:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import "./App.css";
 import NetworkHero from "./components/NetworkHero";
-import DevelopmentServices from "./container/DevelopmentServices";
-import TechnologiesSection from "./container/TechnologiesSection";
-import Faq from "./container/Faq";
-import Industries from "./components/industries/Industries";
-import Testimaonial from "./components/testimonials/Testimonial";
-import Blogs from "./container/BlogSection";
-import DevelomentProcess from "./container/DevelopmentProcesSection";
-import ContactSection from "./components/contact/Contact";
-import WebdevSection from "./container/WebdevSection";
-import DomainExpertise from "./container/DomainExpertise";
-import IndustryToggle from "./components/industrypagecomp/IndustryToggle";
+
 import IndustriesSection from "./container/industry/IndustriesSection";
 function App() {
   return (
