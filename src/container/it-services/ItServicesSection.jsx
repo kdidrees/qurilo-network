ff0bf8e6f4a57a69e18cca59b02150a096d32e40
khurshid <Khurshid@qurilo.com>
2024-07-19 17:41:08 +0530

    updated

--- src/container/it-services/ItServicesSection.jsx
+++ src/container/it-services/ItServicesSection.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { industryPageData } from "../../data/ItServices";
 import ItServices from "../../components/it-services/ItServices";
+import { useMediaQuery } from "react-responsive";
 
 export default function ItServicesSection() {
+  const isTab = useMediaQuery({ maxWidth: 1024 });
+
   return (
     <section>
       {industryPageData.map((item, index) => (
@@ -13,10 +16,15 @@ export default function ItServicesSection() {
           }`}
         >
           <div className="md:m-8 m-2">
-            <img src={item.img} width={600} alt={item.heading} className="rounded-xl" />
+            <img
+              src={item.img}
+              width={isTab ? 1000 : 600}
+              alt={item.heading}
+              className="rounded-xl"
+            />
           </div>
           <div className="ml-0 lg:w-1/2 lg:mt-16 lg:mb-20">
-          <ItServices data={[item]} />
+            <ItServices data={[item]} />
           </div>
         </div>
       ))}
