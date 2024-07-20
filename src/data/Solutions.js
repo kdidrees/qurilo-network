c48fc75aa5bc23199fc7eee6c2c4c6da3e6994bd
khurshid <Khurshid@qurilo.com>
2024-07-20 12:12:43 +0530

    update

---
+++ src/data/Solutions.js
@@ -0,0 +1,163 @@
+// import images here
+import cloudimg from "../assets/it-services/images/cloud_it_services.png";
+import networkimg from "../assets/it-services/images/network_management.png";
+import serverimg from "../assets/it-services/images/server_management.png";
+import cyberimg from "../assets/it-services/images/cyber_security.png";
+import voipimg from "../assets/it-services/images/voip_communication.png";
+import databackupimg from "../assets/it-services/images/data_backup_disaster_recovery.png";
+import helpdeskimg from "../assets/it-services/images/helpdesk_technical_support.png";
+import infrastructure_storageimg from "../assets/it-services/images/infrastructure_storage_services.png";
+import identityimg from "../assets/it-services/images/identity_access_management.png";
+import itconsultingimg from "../assets/it-services/images/it_consulting_strategy.png";
+
+// import hero section images here
+import heroimg from "../assets/it-services/images/heroimg.png";
+import heromobile from "../assets/it-services/images/heromobile.png";
+
+export const heroSectionData = {
+  heroDesktop: heroimg,
+  heroMobile: heromobile,
+  heading: "IT Services",
+  desc: `Providing comprehensive IT solutions to drive business success. Our expertise spans across cloud services, network management, cybersecurity, and more.`,
+};
+
+export const ItServicesPageData = [
+  {
+    id: 1,
+    img: cloudimg,
+    heading: "Enterprise Resource Planning",
+    desc: "Optimize business processes with our ERP software development services. Ensure seamless management of resources, financials, and operations for cost savings and enhanced efficiency.",
+    data: [
+      "Consulting",
+      "Implementation & Migration",
+      "Custom Development",
+      "Testing",
+      "Support and Evolution",
+      "Campaign Management",
+    ],
+  },
+
+  {
+    id: 2,
+    img: networkimg,
+    heading: "Transform Your Supply Chain with SCM Solutions",
+    desc: "Enhance efficiency and optimize your supply chain with our tailored SCM solutions. Streamline logistics, improve inventory management, and boost performance across industries.",
+    data: [
+      "Inventory Management",
+      "Logistics Management",
+      "Procurement Solutions",
+      "Demand Forecasting",
+      "Warehouse Management",
+    ],
+  },
+  {
+    id: 3,
+    img: networkimg,
+    heading: "Financial Management Systems",
+    desc: "Optimize financial operations with our advanced Financial Management Systems. Efficiently manage facilities, improve asset performance, enhance occupant satisfaction, streamline workflows, reduce costs, and ensure compliance.",
+    data: [
+      "Asset Management",
+      "Maintenance Management",
+      "Space Management",
+      "Compliance Management",
+      "Energy Management",
+      "Workplace Analytics",
+    ],
+  },
+  {
+    id: 4,
+    img: networkimg,
+    heading: "Learning Management Systems",
+    desc: "Transform learning with our LMS. Deliver engaging courses, track progress, and optimize training for efficient management and retention.",
+    data: [
+      "Course Creation",
+      "Learner Engagement",
+      "Performance Tracking",
+      "Administration",
+      "Integration",
+    ],
+  },
+  {
+    id: 5,
+    img: networkimg,
+    heading: "Human Resource Management System",
+    desc: "Optimize HR operations with our comprehensive HRMS software solutions. Streamline employee management, payroll processing, and HR tasks to enhance efficiency and employee satisfaction.",
+    data: [
+      "Consulting",
+      "Implementation & Migration",
+      "Custom Development",
+      "Testing",
+      "Support and Evolution",
+      "Employee Engagement",
+    ],
+  },
+  {
+    id: 6,
+    img: networkimg,
+    heading: "Customer Service Solutions",
+    desc: "Enhance customer satisfaction and loyalty with our advanced Customer Service Solutions. Streamline interactions, improve efficiency, and deliver personalized experiences to exceed expectations and achieve operational excellence.",
+    data: [
+      "Support Ticketing System",
+      "CRM Integration",
+      "Multichannel Support",
+      "Self-Service Portals",
+      "Customer Analytics",
+      "Customer Feedback Management",
+    ],
+  },
+  {
+    id: 7,
+    img: networkimg,
+    heading: "Project Management Solutions",
+    desc: "Streamline project workflows and enhance team collaboration with our Project Management Solutions. Optimize planning, execution, and monitoring to deliver projects on time, within budget, and with exceptional quality.",
+    data: [
+      "Project Planning",
+      "Team Collaboration",
+      "Task Management",
+      "Project Reporting",
+      "Risk Management",
+      "Budget Management",
+    ],
+  },
+  {
+    id: 8,
+    img: networkimg,
+    heading: "Customer Relationship Management",
+    desc: "Automate marketing, sales, and customer service with our reliable CRM software development services, ensuring seamless customer tracking, purchase history, and contact management for cost savings and enhanced customer satisfaction.",
+    data: [
+      "Consulting",
+      "Implementation & Migration",
+      "Custom Development",
+      "Testing",
+      "Support and Evolution",
+      "Campaign Management",
+    ],
+  },
+  {
+    id: 9,
+    img: networkimg,
+    heading: "Business Intelligence and Analytics Solutions",
+    desc: "Unlock insights and drive decisions with our custom Business Intelligence and Analytics (BIA) solutions. Integrate data, visualize trends, and monitor performance.",
+    data: [
+      "Consulting",
+      "Data Integration",
+      "Data Visualization",
+      "Predictive Analytics",
+      "BI Reporting",
+      "Support and Maintenance",
+    ],
+  },
+  {
+    id: 10,
+    img: networkimg,
+    heading: "Marketing Automation",
+    desc: "Streamline marketing with our advanced automation solutions. Automate tasks, optimize workflows, and deliver personalized experiences to enhance efficiency, drive engagement, and boost ROI.",
+    data: [
+      "Campaign Management",
+      "Email Marketing Automation",
+      "Lead Nurturing",
+      "Social Media Automation",
+      "CRM Integration",
+    ],
+  },
+];
