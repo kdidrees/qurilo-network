a7adb9f4784c5de87086267c092f4a177b1f4b2b
khurshid <Khurshid@qurilo.com>
2024-07-29 13:49:38 +0530

    update in technologies section data

--- src/container/TechnologiesSection.jsx
+++ src/container/TechnologiesSection.jsx
@@ -16,7 +16,6 @@ const TechnologiesSection = () => {
         following core technologies:`}
       />
    
-
       <div className="flex flex-col justify-center lg:px-20 px-6  py-8">
 
         <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
